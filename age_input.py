0250f356d33231e655523dd088e15d2263b073d2
paria-khalili <120392095+paria-khalili@users.noreply.github.com>
2023-11-10 13:20:45 +0330

    Add files via upload

---
+++ age_input.py
@@ -0,0 +1,16 @@
+age = int(input("enter your age: "))
+
+if age>0 and age<6 :
+    print("khordsal")
+if age>=6 and age<10 :
+    print("koodak")
+if age>=10 and age<14 :
+    print("nojavan")
+if age>=14 and age<24 :
+    print("javan")
+if age>=24 and age<40 :
+    print("bozorgsal")
+if age>=40 :
+    print("miansal")
+
+input("press enter to exit.")
